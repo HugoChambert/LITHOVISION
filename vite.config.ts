81a25ec9f3843c83f5772f85022e3649356fc12a
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2025-12-13 16:00:35 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/LITHOVISION/',
   plugins: [react()],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-  },
-  server: {
-    port: 3000,
-    open: true,
+    emptyOutDir: true,
   },
 })
