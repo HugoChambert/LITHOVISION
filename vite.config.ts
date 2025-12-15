452baa33b98e2a958d192fb20d76e63e7eeee2eb
Hugo_Chambert <149075965+HugoChambert@users.noreply.github.com>
2025-12-15 13:29:52 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  base: '/',
-  plugins: [react()],
+  base: '/LITHOVISION/',
+  server: {
+    port: 3000,
+    open: true
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'pic/**/*',
+          dest: 'pic'
+        },
+        {
+          src: 'Sink/**/*',
+          dest: 'Sink'
+        },
+        {
+          src: 'logo.jpg',
+          dest: ''
+        }
+      ]
+    })
+  ],
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
-  },
-})
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        work: resolve(__dirname, 'work.html'),
+        news: resolve(__dirname, 'news.html'),
+        faq: resolve(__dirname, 'faq.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        warranty: resolve(__dirname, 'warranty.html')
+      }
+    }
+  }
+});
